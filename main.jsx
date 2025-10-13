a88a3751df995f3688bca35eaad0f3a06f204ad0
LeadwithPius <piusbruce595@gmail.com>
2025-10-13 22:52:30 +0300

    caps

--- main.jsx
+++ main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './app';
 import './style.css';
-import './Aurora.css';
+import './aurora';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
